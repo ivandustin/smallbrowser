3fd68210189bc55ef212561d7d29609db290750a
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-05-21 15:16:23 +0800

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name = 'smallbrowser',
     packages = ['smallbrowser'],
-    version = '0.1.9',
+    version = '0.2.0',
     license ='gpl-3.0',
     description = "A small HTTP browser library in Python based on the 'requests' library",
     long_description = long_description,
